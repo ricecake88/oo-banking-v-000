c8ae9b8d73161b95926ed5adc30fe8cdfd79e81f
Grace Shih <vocalise@gmail.com>
2018-05-17 17:16:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ class Transfer
   
   def initialize(sender, receiver, amount)
     @sender = sender,
-    @receiver = to,
+    @receiver = receiver,
     @amount = amount,
     @state = "pending"
   end
