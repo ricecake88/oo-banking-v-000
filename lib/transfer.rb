f2f32095aff520bc13cd066bba982981aa01600d
Grace Shih <vocalise@gmail.com>
2018-05-17 17:26:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,12 @@
+require 'pry'
 class Transfer
   attr_accessor :state, :sender, :receiver
   
   def initialize(sender, receiver, amount)
+    binding.pry
     @sender = sender,
     @receiver = receiver,
     @amount = amount
+    binding.pry
   end
 end
