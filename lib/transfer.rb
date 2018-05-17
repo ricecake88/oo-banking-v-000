89c609980c8ab22c07d439528adc7f38221cd746
Grace Shih <vocalise@gmail.com>
2018-05-17 17:54:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   def execute_transaction
     if @sender.balance < amount
       @status = "rejected"
-      puts "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     elsif @sender.balance >= amount && @status == "pending"
       @sender.balance -= amount
       @receiver.balance += amount
