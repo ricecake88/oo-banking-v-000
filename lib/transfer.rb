3c250cda1605f88675a3f41feee6350b6b3ae64a
Grace Shih <vocalise@gmail.com>
2018-05-17 17:55:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if @receiver.balance >= amount
+    if @receiver.balance >= amount && @status == "complete"
       @sender.balance += amount
       @receiver.balance -= amount
       @status = "reversed"
