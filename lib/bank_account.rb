00e061aef808f72ab7bd9918597cf20e17826bd0
Grace Shih <vocalise@gmail.com>
2018-05-17 17:06:10 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,10 @@
 class BankAccount
   
-  attr_accessor :name, :balance
+  attr_accessor :name, :balance, :state
   def initialize(name)
     @name = name
     @balance = 1000
+    @state = "open"
   end
   
 end
